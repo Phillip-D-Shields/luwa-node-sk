6bdfc6fe8c96350fbeec926bc22fcc8a3f8de13b
phillip shields <matua.phillip.shields@gmail.com>
2023-02-19 21:17:00 +1300

    client: add daisy ui and bottom nav bones

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,12 +1,12 @@
-<h1 class="test">
+<h1 class="text-secondary text-5xl">
   Hello world! 
 </h1>
 
 <style lang="postcss">
-  :global(html) {
+  /* :global(html) {
     background-color: theme(colors.gray.800);
   }
   .test {
     @apply text-3xl font-bold underline text-pink-200;
-  }
+  } */
 </style>
